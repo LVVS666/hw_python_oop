2c7fc4af3ef8944f24a9eb8461a95d2f124ffef5
Василий Логинов <pupok777vasya@gmail.com>
2022-08-03 15:10:07 +0700

    commit_two_send_homework

--- homework.py
+++ homework.py
@@ -75,7 +75,8 @@ class Running(Training):
 
     def get_spent_calories(self) -> float:
         return (
-            (self.COEFF_CALORIE_1 * self.get_mean_speed() - self.COEFF_CALORIE_2)
+            (self.COEFF_CALORIE_1 * self.get_mean_speed()
+             - self.COEFF_CALORIE_2)
             * self.weight
             / self.M_IN_KM
             * (self.duration * self.MINUTE_CONVERT)
@@ -132,7 +133,8 @@ class Swimming(Training):
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        return self.length_pool * self.count_pool / self.M_IN_KM / self.duration
+        return self.length_pool * self.count_pool\
+               / self.M_IN_KM / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
